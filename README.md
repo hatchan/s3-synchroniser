d51bc2b95b4b99a154131caa4ea44257d284004b
Benno van den Berg <bennovandenberg@gmail.com>
2013-12-24 17:07:20 +0100

    wip

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ s3-synchroniser
 ===============
 
 Synchronize two S3 buckets
+
+
+source bucket
+source region
+source aws key id (defaults to use env vars/roles key)
+source aws key secret (defaults to use env vars/roles key)
+
+destination bucket
+destination region (defaults to use the same as source)
+destination aws key id (defaults to use the same as source)
+destination aws key secret (defaults to use the same as source)
---
+++ bin/s3-synchronize.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+var program = require("commander");
+var s3Synchronize = require("../lib/s3-synchornize");
+var util = require("util");
+
+program
+  .version("0.0.1")
+  .option("--sourceAccessKeyId <accessKeyId>", "the accedKeyId for the source bucket (uses $AWS_ACCESS_KEY_ID if not supplied)")
+  .option("--sourceSecretAccessKey <secretAccessKey>", "the secretAccessKey for the source bucket (uses $AWS_SECRET_ACCESS_KEY if not supplied)")
+  .option("--sourceRegion <region>", "the region of the source bucket (uses 'us-east-1' if not supplied)", "us-east-1")
+  .option("--destinationAccessKeyId <accessKeyId>", "the accedKeyId for the destination bucket (uses sourceAccessKeyId if not supplied)")
+  .option("--destinationSecretAccessKey <secretAccessKey>", "the secretAccessKey for the destination bucket (uses sourceSecretAccessKey if not supplied)")
+  .option("--destinationRegion <region>", "the region of the destination bucket (uses sourceRegion if not supplied)")
+  .command("sync <sourceBucket> <destinationBucket>")
+    .action(function(sourceBucket, destinationBucket, command) {
+
+      var options = {
+        "sourceAccessKeyId": program.sourceAccessKeyId,
+        "sourceSecretAccessKey": program.sourceSecretAccessKey,
+        "sourceRegion": program.sourceRegion,
+        "destinationAccessKeyId": program.destinationAccessKeyId,
+        "destinationSecretAccessKey": program.destinationSecretAccessKey,
+        "destinationRegion": program.destinationRegion,
+      };
+
+      s3Synchronize.synchronizeS3Buckets(sourceBucket, destinationBucket, options, function(err) {
+        if (err) {
+          console.error("Unable to synchronize buckets");
+          console.error(util.inspect(err));
+        }
+        else {
+          console.log("Synchronization done");
+        }
+      });
+
+    });
+
+program
+  .parse(process.argv);
